8c165cca71e9c8a528d91147aa80d339283b0af7
Guacamole1234 <thegamerlapalma@gmail.com>
2024-02-08 16:00:42 +0000

    08/02/24

---
+++ Assets/BotonSuicidio.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FuncionesUI : MonoBehaviour
+{
+    public void FuncionBoton()
+    {
+        Debug.Log("La culpa no era mía");
+    }
+    public void FuncionConParametros(int cuantos)
+    {
+        Debug.Log("ni donde estaba ni como vestía, el violador son " + cuantos + " enanos de circo");
+    }
+}
